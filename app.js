c753760f842784a97bfb04c4cade76379977e88e
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-23 11:18:37 -0400

    implements show and create

--- app.js
+++ app.js
@@ -16,14 +16,47 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded( {extended: true} ))
+
 app.get('/', (req, res) => {
   res.render('home')
+
 })
 
+//index
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
 })
+
+//new - needs to go before show because show will treat "new" as ":id"
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+//endpoint where form is submitted to
+app.post('/campgrounds', async (req, res) => {
+  console.log(req.body)
+  //need req.body.campground because in our forms we named the fields campground[title]
+  const newCampground = new Campground(req.body.campground)
+  await newCampground.save()
+  res.redirect(`/campgrounds/${newCampground._id}`)
+})
+
+//show
+app.get('/campgrounds/:id', async(req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/show', { campground })
+})
+
+//edit - gets the edit form
+app.get('/campgrounds/:id/edit', async(req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/edit', {campground})
+})
+
 
 app.listen(3000, () => {
   console.log("LISTENING ON 3000")
